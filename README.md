6881de62211056e3fb60d2726401eb8c8d8c1826
Fabien Bellanger <valentil@gmail.com>
2020-01-29 23:27:46 +0100

    Add some functions

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # go-code-analyser
-A little code analyser written in Go.
+A little code analyser written in Go. Inspired by [gocloc](https://github.com/hhatto/gocloc).
 
 [![Build Status](https://travis-ci.org/fabienbellanger/goCodeAnalyser.svg?branch=master)](https://travis-ci.org/fabienbellanger/goCodeAnalyser)
 [![Go Report Card](https://goreportcard.com/badge/github.com/fabienbellanger/goCodeAnalyser)](https://goreportcard.com/report/github.com/fabienbellanger/goCodeAnalyser)
--- file/file.go
+++ file/file.go
@@ -1,1 +1,32 @@
 package file
+
+import (
+	"os"
+	"strings"
+)
+
+// isVCSDir checks if directory is a version control system.
+func isVCSDir(path string) bool {
+	if len(path) > 1 && path[0] == os.PathSeparator {
+		path = path[1:]
+	}
+	vcsDirs := []string{".bzr", ".cvs", ".hg", ".git", ".svn"}
+	for _, dir := range vcsDirs {
+		if strings.Contains(path, dir) {
+			return true
+		}
+	}
+	return false
+}
+
+// trimBOM trims BOM (UTF-8) of a line.
+func trimBOM(line string) string {
+	l := len(line)
+	if l >= 3 {
+		if line[0] == 0xef && line[1] == 0xbb && line[2] == 0xbf {
+			trimLine := line[3:]
+			return trimLine
+		}
+	}
+	return line
+}
